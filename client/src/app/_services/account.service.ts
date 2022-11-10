bf07cf5ab92fb8b5276c9b8314f3e5ec64ee93c9
Pedro Delgado <pedro@pedrodelgado.ca>
2022-11-10 11:53:44 -0500

    PhotoGallery

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { stringify } from 'querystring';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 
@@ -10,7 +11,7 @@ import { User } from '../_models/user';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl= 'https://localhost:5001/api/';
+  baseUrl= environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
