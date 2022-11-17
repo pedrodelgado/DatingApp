b2c5c8c55d61c0b49c8b5c17df8f810508f569f5
Pedro Delgado <pedro@pedrodelgado.ca>
2022-11-17 13:01:09 -0500

    -Caching -Timeago -ActionFilters, IQueryable

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : IUserRepository 
     {
         private readonly DataContext  _context;
         private readonly IMapper _mapper;
@@ -27,11 +28,26 @@ namespace API.Data
             .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
             .SingleOrDefaultAsync();
         }
-        public async Task<IEnumerable<MemberDTO>> GetMembersAsync()
+        public async Task<PagedList<MemberDTO>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
-            .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
-            .ToListAsync();
+            var query = _context.Users.AsQueryable();
+
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
+
+            return await PagedList<MemberDTO>.CreateAsync(query.ProjectTo<MemberDTO>(_mapper.ConfigurationProvider).AsNoTracking(),
+            userParams.PageNumber,userParams.PageSize);
         }
         
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
