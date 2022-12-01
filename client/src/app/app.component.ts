c0484fd8ffbe573585741dca8ad5f74b8b555a72
Pedro Delgado <pedro@pedrodelgado.ca>
2022-12-01 00:19:04 -0500

    -fixed null setCurrentUser

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -20,7 +20,9 @@ export class AppComponent implements OnInit {
 }
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
+    if(user){
     this.accountService.setCurrentUser(user);
+    }
   }
 
 }
